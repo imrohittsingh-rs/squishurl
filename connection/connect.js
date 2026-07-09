322d61644cc34b5a5c94703a37d1353c490c247c
Rohit Singh <imrohittsingh.rs@gmail.com>
2026-07-09 13:51:39 +0530

    fix some minor changes

--- connection/connect.js
+++ connection/connect.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
 async function connectToMongoDb(url) {
-  mongoose.connect(url);
+  try {
+    await mongoose.connect(url);
+    console.log("Connected to MongoDB successfully");
+  } catch (err) {
+    console.error("MongoDB connection error", err);
+    process.exit(1);
+  }
 }
 
 module.exports = connectToMongoDb;
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = 8001;
+
 
 const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv")
+const dns = require("dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+dotenv.config()
+const PORT = process.env.PORT || 8000;
 
 const urlRoute = require("./routes/url");
 const staticRouter = require("./routes/staticRouter");
@@ -15,13 +21,14 @@ const connectToMongoDb = require("./connection/connect");
 const path = require("path");
 
 // Connecting to MongoDb
-connectToMongoDb("mongodb://localhost:27017/short-url")
+connectToMongoDb(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDb"))
   .catch((err) => console.log("Failed to connect", err));
 
 // Setting up view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.locals.PORT = PORT;
 
 // MiddleWares
 app.use(express.json());
@@ -40,5 +47,5 @@ app.use(authMiddleware);
 app.use("/url", urlRoute);
 
 app.listen(PORT, () => {
-  console.log(`Server is listning on PORT: ${PORT}`);
+  console.log(`Server is listening on PORT: ${PORT}`);
 });
