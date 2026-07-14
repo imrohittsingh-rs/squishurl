c1d6ab9e7dd6ba599cab75e385ec12ef815ef147
Rohit Singh <imrohittsingh.rs@gmail.com>
2026-07-14 22:09:23 +0530

    fix cookie options

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -44,7 +44,7 @@ const handleUserLogin = asyncHandler(async (req, res) => {
   res.cookie(cookieName, token, {
     httpOnly: true,
     secure: isProduction,
-    sameSite: isProduction ? "strict" : "lax",
+    sameSite: isProduction ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
@@ -60,7 +60,7 @@ const handleUserSignOut = asyncHandler(async (req, res) => {
   res.clearCookie(cookieName, {
     httpOnly: true,
     secure: isProduction,
-    sameSite: isProduction ? "strict" : "lax",
+    sameSite: isProduction ? "none" : "lax",
   });
   return res
     .status(200)
